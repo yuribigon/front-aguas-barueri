543fa31af332075a081f18d5a7b6ea1b926bfcb5
Yuri Bigon <yuri.bigon@gmail.com>
2023-06-12 16:53:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import fakeData from "./MOCK_DATA.json";
+import * as React from "react";
+import { useTable } from "react-table";
 
 function App() {
+  const data = React.useMemo(() => fakeData, []);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "CNPJ",
+        accessor: "cnpj",
+      },
+      {
+        Header: "Razão Social",
+        accessor: "razao_social",
+      },
+      {
+        Header: "Nome Fantasia",
+        accessor: "nome_fantasia",
+      },
+      {
+        Header: "Contato",
+        accessor: "contact",
+      },
+      {
+        Header: "Valor (m\u00b3)",
+        accessor: "value",
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  const newForm = document.querySelector('.new-form');
+  const editForm = document.querySelector('.edit-form');
+
+  function showForm() {
+    if(newForm.style.display === 'block'){
+      newForm.style.display = 'none';
+    } else {
+      newForm.style.display = 'block';
+    }
+  }
+
+  function showEditForm() {
+    if(editForm.style.display === 'block'){
+      editForm.style.display = 'none';
+    } else {
+      editForm.style.display = 'block';
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="new-form">
+        Novo cadastro
+      </div>
+      <div className="edit-form">
+        Editar cadastro
+      </div>
+      <div className="container">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <button id="btn-edit" className="btn-edit" onClick={showEditForm}>
+        <span class="material-symbols-outlined">
+          edit
+        </span>
+      </button>
+      <button id="btn-new" className="btn-new" onClick={showForm}>
+        <span class="material-symbols-outlined">
+          add
+        </span>
+      </button>
     </div>
   );
 }
